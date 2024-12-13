44856fd8975f132867bcced65d351d084ce63955
Abraao Braz De Oliveira <abraz@uolinc.com>
2024-12-13 16:14:01 -0300

    Criação do card e temificação da app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
+import { Card } from "./componentes/Card/Card";
+import { ProvedorTema } from "./componentes/ProvedorTema/ProvedorTema";
 import { Estilo } from "./EstilosGlobais/Estilo";
 
 function App() {
   return (
-    <div>
+    <ProvedorTema>
       <Estilo />
-      <h1>freelando</h1>
-    </div>
+      <Card>
+        <h1>freelando</h1>
+      </Card>
+    </ProvedorTema>
   );
 }
 
---
+++ src/componentes/ProvedorTema/ProvedorTema.jsx
@@ -0,0 +1,48 @@
+import { ThemeProvider } from '@emotion/react'
+
+const tema = {
+    cores: {
+        branco:'',
+        primarias: {
+            a:'#5754ED',
+            b:'',
+            c:''
+        },
+        secundarias: {
+            a:'#EBEAF9',
+            b:'',
+            c:''
+        },
+        neutras: {
+            a:'',
+            b:'',
+            c:''
+        },
+        dark: {
+            a:'',
+            b:''
+        },
+        warning: '',
+        focus: ''
+    },
+    espacamentos: {
+        xxs: '4px',
+        xs: '8px',
+        s: '16px',
+        m: '24px',
+        l: '32px',
+        xl: '48px',
+        xxl: '64px'
+
+    }
+    
+}
+
+export const ProvedorTema = ({ children }) => {
+    return (
+        <ThemeProvider theme={tema}>
+            {children}
+        </ThemeProvider>
+    )
+} 
+
